aa2e8a37dbf7d400c39b4dba87c35cae5af32f14
Novem <128050596+Novem07@users.noreply.github.com>
2023-12-14 18:47:21 +0700

    almostdone

--- Tetris.cpp
+++ Tetris.cpp
@@ -1,251 +1,298 @@
 #include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <conio.h>
-#include <array>
-#include <chrono>
 #include <thread>
+#include <vector>
+#include <windows.h>
 
 using namespace std;
 
-const int BOARD_WIDTH = 10;
-const int BOARD_HEIGHT = 20;
+class TetrisRenderer {
+protected:
+    int nScreenWidth;
+    int nScreenHeight;
+    wchar_t* screen;
+    HANDLE hConsole;
+    DWORD dwBytesWritten = 0;
 
-class TetrisGame {
 public:
-    TetrisGame();
-    void run();
+    TetrisRenderer(int screenWidth, int screenHeight);
+    ~TetrisRenderer();
 
-private:
-    char board[BOARD_HEIGHT][BOARD_WIDTH];
-    int score;
-    bool gameover;
-
-    struct Point {
-        int x, y;
-    };
-
-    struct Tetromino {
-        Point blocks[4];
-    };
-
-    Tetromino currentTetromino;
-    int currentRotation;
-
-    vector<Tetromino> tetrominos;
-
-    void initBoard();
-    void drawBoard();
-    void drawTetromino();
-    void clearTetromino();
-    bool isValidMove(int offsetX, int offsetY, int rotation);
-    void placeTetromino();
-    void checkLines();
-    void spawnTetromino();
-    void handleInput();
+    void Draw();
 };
 
-TetrisGame::TetrisGame() : score(0), gameover(false), currentRotation(0) {
-    srand(static_cast<unsigned>(time(0)));
+class TetrisGame : public TetrisRenderer{
+private:
+    int nFieldWidth;
+    int nFieldHeight;
+    unsigned char* pField;
+    vector<wstring> tetromino;
+    int nCurrentPiece;
+    int nCurrentRotation;
+    int nCurrentX;
+    int nCurrentY;
+    int nSpeed;
+    int nSpeedCount;
+    bool bForceDown;
+    bool bRotateHold;
+    int nPieceCount;
+    int nScore;
+    vector<int> vLines;
+    bool bGameOver;
 
-    initBoard();
+    int Rotate(int px, int py, int r);
+    bool DoesPieceFit(int nTetromino, int nRotation, int nPosX, int nPosY);
+    //void ClearLines();
 
-    tetrominos.push_back({{{0, 1}, {1, 1}, {2, 1}, {3, 1}}}); // I
-    tetrominos.push_back({{{1, 0}, {0, 1}, {1, 1}, {2, 1}}}); // J
-    tetrominos.push_back({{{0, 1}, {1, 1}, {2, 1}, {2, 0}}}); // L
-    tetrominos.push_back({{{0, 0}, {0, 1}, {1, 0}, {1, 1}}}); // O
-    tetrominos.push_back({{{1, 0}, {2, 0}, {0, 1}, {1, 1}}}); // S
-    tetrominos.push_back({{{0, 1}, {1, 1}, {2, 1}, {1, 0}}}); // T
-    tetrominos.push_back({{{0, 0}, {1, 0}, {1, 1}, {2, 1}}}); // Z
+public:
+    TetrisGame(int width, int height, int screenWidth, int screenHeight);
+    ~TetrisGame();
 
-    spawnTetromino();
+    void RunGame();
+};
+
+
+int TetrisGame::Rotate(int px, int py, int r)
+{
+	int pi = 0;
+	switch (r % 4)
+	{
+	case 0: // 0 degrees			// 0  1  2  3
+		pi = py * 4 + px;			// 4  5  6  7
+		break;						// 8  9 10 11
+									//12 13 14 15
+
+	case 1: // 90 degrees			//12  8  4  0
+		pi = 12 + py - (px * 4);	//13  9  5  1
+		break;						//14 10  6  2
+									//15 11  7  3
+
+	case 2: // 180 degrees			//15 14 13 12
+		pi = 15 - (py * 4) - px;	//11 10  9  8
+		break;						// 7  6  5  4
+									// 3  2  1  0
+
+	case 3: // 270 degrees			// 3  7 11 15
+		pi = 3 - py + (px * 4);		// 2  6 10 14
+		break;						// 1  5  9 13
+	}								// 0  4  8 12
+
+	return pi;
 }
 
-void TetrisGame::initBoard() {
-    for (int i = 0; i < BOARD_HEIGHT; ++i) {
-        for (int j = 0; j < BOARD_WIDTH; ++j) {
-            board[i][j] = ' ';
-        }
+bool TetrisGame::DoesPieceFit(int nTetromino, int nRotation, int nPosX, int nPosY)
+{
+	// All Field cells >0 are occupied
+	for (int px = 0; px < 4; px++)
+		for (int py = 0; py < 4; py++)
+		{
+			// Get index into piece
+			int pi = Rotate(px, py, nRotation);
+
+			// Get index into field
+			int fi = (nPosY + py) * nFieldWidth + (nPosX + px);
+
+			// Check that test is in bounds. Note out of bounds does
+			// not necessarily mean a fail, as the long vertical piece
+			// can have cells that lie outside the boundary, so we'll
+			// just ignore them
+			if (nPosX + px >= 0 && nPosX + px < nFieldWidth)
+			{
+				if (nPosY + py >= 0 && nPosY + py < nFieldHeight)
+				{
+					// In Bounds so do collision check
+					if (tetromino[nTetromino][pi] != L'.' && pField[fi] != 0)
+						return false; // fail on first hit
+				}
+			}
+		}
+
+	return true;
+}
+
+// TetrisGame implementation
+TetrisGame::TetrisGame(int width, int height, int screenWidth, int screenHeight)
+    : TetrisRenderer(screenWidth, screenHeight), // Pass screenWidth and screenHeight to TetrisRenderer constructor
+      nFieldWidth(width),
+      nFieldHeight(height),
+      pField(nullptr),
+      tetromino(7),
+      nCurrentPiece(0),
+      nCurrentRotation(0),
+      nCurrentX(nFieldWidth / 2),
+      nCurrentY(0),
+      nSpeed(20),
+      nSpeedCount(0),
+      bForceDown(false),
+      bRotateHold(true),
+      nPieceCount(0),
+      nScore(0),
+      bGameOver(false) {
+    pField = new unsigned char[nFieldWidth * nFieldHeight];
+    for (int x = 0; x < nFieldWidth; x++)
+        for (int y = 0; y < nFieldHeight; y++)
+            pField[y * nFieldWidth + x] = (x == 0 || x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9 : 0;
+
+    // Initialize tetromino shapes
+    tetromino[0].append(L"..X...X...X...X.");
+    tetromino[1].append(L"..X..XX...X.....");
+    tetromino[2].append(L".....XX..XX.....");
+    tetromino[3].append(L"..X..XX..X......");
+    tetromino[4].append(L".X...XX...X.....");
+    tetromino[5].append(L".X...X...XX.....");
+    tetromino[6].append(L"..X...X..XX.....");
+}
+
+
+TetrisGame::~TetrisGame() {
+    delete[] pField;
+}
+
+void TetrisGame::RunGame() {
+    bool bKey[4];
+	vector<int> vLines;
+	//bool bGameOver = false;
+    while (!bGameOver) {
+        // Game logic goes here
+        // Timing =======================
+		this_thread::sleep_for(50ms); // Small Step = 1 Game Tick
+		nSpeedCount++;
+		bForceDown = (nSpeedCount == nSpeed);
+
+		// Input ========================
+		for (int k = 0; k < 4; k++)								// R   L   D Z
+			bKey[k] = (0x8000 & GetAsyncKeyState((unsigned char)("\x27\x25\x28Z"[k]))) != 0;
+		
+		// Game Logic ===================
+
+		// Handle player movement
+		nCurrentX += (bKey[0] && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX + 1, nCurrentY)) ? 1 : 0;
+		nCurrentX -= (bKey[1] && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX - 1, nCurrentY)) ? 1 : 0;		
+		nCurrentY += (bKey[2] && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY + 1)) ? 1 : 0;
+
+		// Rotate, but latch to stop wild spinning
+		if (bKey[3])
+		{
+			nCurrentRotation += (bRotateHold && DoesPieceFit(nCurrentPiece, nCurrentRotation + 1, nCurrentX, nCurrentY)) ? 1 : 0;
+			bRotateHold = false;
+		}
+		else
+			bRotateHold = true;
+
+		// Force the piece down the playfield if it's time
+		if (bForceDown)
+		{
+			// Update difficulty every 50 pieces
+			nSpeedCount = 0;
+			nPieceCount++;
+			if (nPieceCount % 50 == 0)
+				if (nSpeed >= 10) nSpeed--;
+			
+			// Test if piece can be moved down
+			if (DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY + 1))
+				nCurrentY++; // It can, so do it!
+			else
+			{
+				// It can't! Lock the piece in place
+				for (int px = 0; px < 4; px++)
+					for (int py = 0; py < 4; py++)
+						if (tetromino[nCurrentPiece][Rotate(px, py, nCurrentRotation)] != L'.')
+							pField[(nCurrentY + py) * nFieldWidth + (nCurrentX + px)] = nCurrentPiece + 1;
+
+				// Check for lines
+				for (int py = 0; py < 4; py++)
+					if(nCurrentY + py < nFieldHeight - 1)
+					{
+						bool bLine = true;
+						for (int px = 1; px < nFieldWidth - 1; px++)
+							bLine &= (pField[(nCurrentY + py) * nFieldWidth + px]) != 0;
+
+						if (bLine)
+						{
+							// Remove Line, set to =
+							for (int px = 1; px < nFieldWidth - 1; px++)
+								pField[(nCurrentY + py) * nFieldWidth + px] = 8;
+							vLines.push_back(nCurrentY + py);
+						}						
+					}
+
+				nScore += 25;
+				if(!vLines.empty())	nScore += (1 << vLines.size()) * 100;
+
+				// Pick New Piece
+				nCurrentX = nFieldWidth / 2;
+				nCurrentY = 0;
+				nCurrentRotation = 0;
+				nCurrentPiece = rand() % 7;
+
+				// If piece does not fit straight away, game over!
+				bGameOver = !DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY);
+			}
+		}
+
+		// Display ======================
+
+		// Draw Field
+		for (int x = 0; x < nFieldWidth; x++)
+			for (int y = 0; y < nFieldHeight; y++)
+				screen[(y + 2)*nScreenWidth + (x + 2)] = L" ABCDEFG=#"[pField[y*nFieldWidth + x]];
+
+		// Draw Current Piece
+		for (int px = 0; px < 4; px++)
+			for (int py = 0; py < 4; py++)
+				if (tetromino[nCurrentPiece][Rotate(px, py, nCurrentRotation)] != L'.')
+					screen[(nCurrentY + py + 2)*nScreenWidth + (nCurrentX + px + 2)] = nCurrentPiece + 65;
+
+		// Draw Score
+		swprintf_s(&screen[2 * nScreenWidth + nFieldWidth + 6], 16, L"SCORE: %8d", nScore);
+
+		// Animate Line Completion
+		if (!vLines.empty())
+		{
+			// Display Frame (cheekily to draw lines)
+			WriteConsoleOutputCharacterW(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+			this_thread::sleep_for(400ms); // Delay a bit
+
+			for (auto &v : vLines)
+				for (int px = 1; px < nFieldWidth - 1; px++)
+				{
+					for (int py = v; py > 0; py--)
+						pField[py * nFieldWidth + px] = pField[(py - 1) * nFieldWidth + px];
+					pField[px] = 0;
+				}
+
+			vLines.clear();
+		}
+
+		// Display Frame
+		WriteConsoleOutputCharacterW(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
     }
 }
 
-void TetrisGame::drawBoard() {
-    system("cls"); // Clear the console
+// TetrisRenderer implementation
 
-    for (int i = 0; i < BOARD_HEIGHT; ++i) {
-        for (int j = 0; j < BOARD_WIDTH; ++j) {
-            cout << board[i][j];
-        }
-        cout << endl;
-    }
-
-    cout << "Score: " << score << endl;
+TetrisRenderer::TetrisRenderer(int screenWidth, int screenHeight)
+    : nScreenWidth(screenWidth), nScreenHeight(screenHeight) {
+    screen = new wchar_t[nScreenWidth * nScreenHeight];
+    for (int i = 0; i < nScreenWidth * nScreenHeight; i++) screen[i] = L' ';
+    hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
 }
 
-void TetrisGame::drawTetromino() {
-    for (int i = 0; i < 4; ++i) {
-        int x = currentTetromino.blocks[i].x;
-        int y = currentTetromino.blocks[i].y;
-        board[y][x] = '*';
-    }
+TetrisRenderer::~TetrisRenderer() {
+    delete[] screen;
+    CloseHandle(hConsole);
 }
 
-void TetrisGame::clearTetromino() {
-    for (int i = 0; i < 4; ++i) {
-        int x = currentTetromino.blocks[i].x;
-        int y = currentTetromino.blocks[i].y;
-        board[y][x] = ' ';
-    }
+void TetrisRenderer::Draw() {
+    // Drawing logic goes here
 }
 
-bool TetrisGame::isValidMove(int offsetX, int offsetY, int rotation) {
-    clearTetromino();
-
-    for (int i = 0; i < 4; ++i) {
-        int x = currentTetromino.blocks[i].x + offsetX;
-        int y = currentTetromino.blocks[i].y + offsetY;
-
-        // Check if the new position is within the board bounds
-        if (x < 0 || x >= BOARD_WIDTH || y < 0 || y >= BOARD_HEIGHT) {
-            return false;
-        }
-
-        // Check if the new position is occupied by another block
-        if (board[y][x] != ' ') {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-void TetrisGame::placeTetromino() {
-    for (int i = 0; i < 4; ++i) {
-        int x = currentTetromino.blocks[i].x;
-        int y = currentTetromino.blocks[i].y;
-        board[y][x] = '*';
-    }
-}
-
-void TetrisGame::checkLines() {
-    for (int i = BOARD_HEIGHT - 1; i >= 0; --i) {
-        bool lineFull = true;
-
-        for (int j = 0; j < BOARD_WIDTH; ++j) {
-            if (board[i][j] == ' ') {
-                lineFull = false;
-                break;
-            }
-        }
-
-        if (lineFull) {
-            // Remove the line
-            for (int k = i; k > 0; --k) {
-                for (int j = 0; j < BOARD_WIDTH; ++j) {
-                    board[k][j] = board[k - 1][j];
-                }
-            }
-
-            // Increment the score
-            score += 10;
-        }
-    }
-}
-
-void TetrisGame::spawnTetromino() {
-    currentTetromino = tetrominos[rand() % tetrominos.size()];
-    currentRotation = 0;
-
-    if (!isValidMove(0, 0, currentRotation)) {
-        gameover = true;
-    }
-}
-
-void TetrisGame::handleInput() {
-    int newRotation;  // Di chuyển khai báo ra ngoài switch-case
-
-    if (_kbhit()) {
-        char key = _getch();
-
-        switch (key) {
-            case 'a':
-                if (isValidMove(-1, 0, currentRotation)) {
-                    clearTetromino();
-                    --currentTetromino.blocks[0].x;
-                    --currentTetromino.blocks[1].x;
-                    --currentTetromino.blocks[2].x;
-                    --currentTetromino.blocks[3].x;
-                    drawTetromino();
-                }
-                break;
-
-            case 'd':
-                if (isValidMove(1, 0, currentRotation)) {
-                    clearTetromino();
-                    ++currentTetromino.blocks[0].x;
-                    ++currentTetromino.blocks[1].x;
-                    ++currentTetromino.blocks[2].x;
-                    ++currentTetromino.blocks[3].x;
-                    drawTetromino();
-                }
-                break;
-
-            case 's':
-                if (isValidMove(0, 1, currentRotation)) {
-                    clearTetromino();
-                    ++currentTetromino.blocks[0].y;
-                    ++currentTetromino.blocks[1].y;
-                    ++currentTetromino.blocks[2].y;
-                    ++currentTetromino.blocks[3].y;
-                    drawTetromino();
-                }
-                break;
-
-            case 'w':
-                // Di chuyển khởi tạo vào đây
-                newRotation = (currentRotation + 1) % 4;
-                if (isValidMove(0, 0, newRotation)) {
-                    clearTetromino();
-                    currentRotation = newRotation;
-                    drawTetromino();
-                }
-                break;
-
-            case 'q':
-                gameover = true;
-                break;
-        }
-    }
-}
-
-void TetrisGame::run() {
-    while (!gameover) {
-        drawBoard();
-        handleInput();
-
-        if (isValidMove(0, 1, currentRotation)) {
-            clearTetromino();
-            ++currentTetromino.blocks[0].y;
-            ++currentTetromino.blocks[1].y;
-            ++currentTetromino.blocks[2].y;
-            ++currentTetromino.blocks[3].y;
-            drawTetromino();
-        } else {
-            placeTetromino();
-            checkLines();
-            spawnTetromino();
-        }
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));  // Delay 100 milliseconds
-    }
-
-    cout << "Game Over! Your score: " << score << endl;
-}
+// Main function
 
 int main() {
-    TetrisGame game;
-    game.run();
+    TetrisRenderer renderer(120, 30);
+    TetrisGame tetrisGame(10, 20, 120, 30);
+    tetrisGame.RunGame();
 
     return 0;
 }
